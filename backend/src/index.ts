fae5dfbc18650479a28995aafaa443c1f28ac647
Meeth Patel <meeth2060@gmail.com>
2025-07-19 12:12:45 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post(' /api/v1/user/signup',(c)=>{
+  return c.text("signup route")
+})
+
+app.post('/api/v1/user/signin',(c)=>{
+  return c.text('signin route')
+})
+
+app.post(' /api/v1/blog',(c)=>{
+  return c.text('blog site post')
+})
+
+app.put('/api/v1/blog/:id',(c)=>{
+  const id = c.req.param('id')
+ 	console.log(id);
+  return c.text('blog put')
+})
+
+app.put('/api/v1/blog/:id',(c)=>{
+  return c.text('blog of a specific person')
+})
+
+app.get(' /api/v1/blog/bulk',(c)=>{
+  return c.text('last route')
+})
+
+export default app
